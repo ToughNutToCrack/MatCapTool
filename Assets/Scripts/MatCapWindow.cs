7fe85f444abdfb2d806d0ffd2fe2d2a17781df1d
doJester13 <minarini.work@gmail.com>
2021-05-20 15:43:09 +0200

    preview size

--- Assets/Scripts/MatCapWindow.cs
+++ Assets/Scripts/MatCapWindow.cs
@@ -5,10 +5,11 @@ using System;
 
 public class MatCapWindow : EditorWindow{
     const int height = 70;
-    public static int size = 512;
+    public static int previewSize = 512;
     public string textureName = "MatCapTexture";
 
     float progress = -1;
+    public int size = 512;
     string progressMsg = "Encoding texture...";
     string basePathTextures = "Assets/Resources/Textures";
     GameObject gameObject;
@@ -21,12 +22,12 @@ public class MatCapWindow : EditorWindow{
         window = GetWindow<MatCapWindow>(false, "GenerateTexture", true);
         window.resizeWindow();
         window.Show();
-        window.position = new Rect(0, 0, size, height);
+        window.position = new Rect(0, 0, previewSize, height);
         window.init();
     }
 
     void resizeWindow(){
-        Vector2 dim = new Vector2(size, size + height);
+        Vector2 dim = new Vector2(previewSize, previewSize + height);
         window.minSize = dim;
     }
  
@@ -46,17 +47,16 @@ public class MatCapWindow : EditorWindow{
     void OnGUI(){
         textureName = EditorGUILayout.TextField("Texture Name", textureName);
         size = EditorGUILayout.IntField("Image Size", size);
-        window.resizeWindow();
 
         cam.targetTexture = new RenderTexture(
-            size,
-            size, 
+            previewSize,
+            previewSize, 
             24,
             RenderTextureFormat.ARGB32
         );        
 
         cam.Render();
-        GUI.DrawTexture(new Rect(0, height, size, size), cam.targetTexture);
+        GUI.DrawTexture(new Rect(0, height, previewSize, previewSize), cam.targetTexture);
 
         if (GUILayout.Button("Save")){
             textureName = string.IsNullOrEmpty(textureName)? DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") : textureName;
